a4d6ef9afa1ec973f10bd1ec0c32d8e0ffbe540d
Brent Morrison <brent.morrison@hotmail.com>
2020-04-11 23:02:49 +0100

    Add date conversion

--- edgar_import.py
+++ edgar_import.py
@@ -1,10 +1,24 @@
 from sqlalchemy import create_engine
 import pandas as pd
+import numpy as np
+import datetime as dt
 
-engine = create_engine('postgresql://postgres:pass@localhostlocalhost:5432/stock_master')
+# View columns of file to be read in
 cols = list(pd.read_csv('C:/Users/brent/Downloads/num.txt', delimiter = '\t', nrows = 1))
-df = pd.read_csv('C:/Users/brent/Downloads/num.txt', delimiter = '\t') #usecols =[i for i in cols if i != 'footnote'])
-df.to_sql('pandas_db', engine)
-
 print(cols)
-#Test
+
+# Read file
+# If required, to exlude columns (usecols =[i for i in cols if i != 'footnote'])
+df = pd.read_csv('C:/Users/brent/Downloads/num.txt', delimiter = '\t') 
+
+~ Subset for test
+df_test = df.iloc[:9]
+
+df_test.info()
+
+# Convert date
+df_test['ddate_new'] = pd.to_datetime(df_test['ddate'], format='%Y%m%d').dt.strftime("%Y-%m-%d")
+
+# Convert date
+engine = create_engine('postgresql://postgres:*******@localhost:5432/stock_master')
+df.to_sql('edgar.num_stage', engine)
